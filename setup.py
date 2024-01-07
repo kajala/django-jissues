b2e4e799b7248281423405dc129401566065b451
Jani Kajala <kajala@gmail.com>
2024-01-07 17:34:44 -0500

    Release 1.0.4

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requires = parse_requirements("requirements.txt", session=False)
 
 setup(
     name="django-jissues",
-    version="1.0.3",
+    version="1.0.4",
     author="Jani Kajala",
     author_email="kajala@gmail.com",
     packages=find_packages(exclude=["project", "venv"]),
